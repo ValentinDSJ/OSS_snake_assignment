522d4cb297a2503ebb9565862576c1a20dff4f7b
Amaury LECOMTE <amaury.lecomte1@gmail.com>
2022-04-20 19:02:35 +0900

    🐛 Fixed resize window

--- src/js/prefabs/SharedPrefabs.ts
+++ src/js/prefabs/SharedPrefabs.ts
@@ -5,7 +5,7 @@ export default class SharedPrefabs {
     static createApplication(app: Application): Array<Component> {
         let components = Array<Component>();
 
-        app.app = new PIXI.Application({view: document.querySelector("#game")});
+        app.app = new PIXI.Application({view: document.querySelector("#game"), resizeTo: window});
         components.push(app);
         return components;
     }
