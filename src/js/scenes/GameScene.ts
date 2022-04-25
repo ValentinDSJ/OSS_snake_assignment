5ec327235622dcd7cc566bdf22927b35022692ee
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2022-04-25 11:02:25 +0900

    Added handling for screen resolution

--- src/js/scenes/GameScene.ts
+++ src/js/scenes/GameScene.ts
@@ -21,6 +21,8 @@ export default class GameScene extends Scene {
     this.initEntity(GamePrefabs.createBoard());
     this.initEntity(GamePrefabs.createButton());
     this.initEntity(GamePrefabs.createHead());
-    // this.initEntity(GamePrefabs.createApple(300, 200));
+    this.initEntity(
+      GamePrefabs.createApple(Math.random() * 500, Math.random() * 500)
+    );
   }
 }
